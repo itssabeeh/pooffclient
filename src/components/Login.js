c90ff595985a1cf5cee25cba9fff254801ac7c46
layone <itssabeeh@gmail.com>
2021-08-22 00:14:31 +0530

    basic functionality

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,68 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Redirect } from 'react-router-dom';
 
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState();
+  const [isError, setIsError] = useState(false);
+  const [redirect, setRedirect] = useState();
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(username, password);
+    if (!isLogin) {
+      postData();
+    } else {
+      postLogin();
+    }
     setUsername('');
     setPassword('');
   };
+  const postLogin = async () => {
+    try {
+      const record = await axios.post('http://localhost:8000/login', {
+        username,
+        password,
+      });
+      setData(record.data);
+      console.log(record.data);
+      localStorage.setItem('userInfo', JSON.stringify(record.data));
+      setIsLoading(false);
+      setRedirect(true);
+      if (record.data.status === 'error') {
+        alert(record.data.error);
+        setIsError(true);
+      }
+    } catch (error) {
+      setIsError(true);
+    }
+  };
+
+  const postData = async () => {
+    try {
+      const record = await axios.post('http://localhost:8000/register', {
+        username,
+        password,
+      });
+      setData(record.data);
+      console.log(record.data);
+      setIsLoading(false);
+      if (record.data.status === 'error') {
+        alert(record.data.error);
+      }
+      setRedirect(true);
+    } catch (error) {
+      setIsError(true);
+    }
+  };
+
+  if (redirect) {
+    return <Redirect to="/Home" />;
+  }
+
   return (
     <>
       <div>
